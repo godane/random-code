37eceb153f502a9f8564a564b858df6fa766e0c1
Christopher Rogers <slaxemulator@gmail.com>
2020-07-19 19:42:37 -0400

    Add archive.org trackers to mktorrent.sh.

--- internet-archive/mktorrent.sh
+++ internet-archive/mktorrent.sh
@@ -4,7 +4,9 @@ dir="$1"
 
 if [ -d "$dir" ]; then
 
-mktorrent -a udp://tracker.publicbt.com:80/announce \
+mktorrent -a http://bt1.archive.org:6969/announce \
+	-a http://bt2.archive.org:6969/announce \
+	-a udp://tracker.publicbt.com:80/announce \
 	-a udp://tracker.openbittorrent.com:80 \
 	-a udp://tracker.ccc.de:80 \
 	-a udp://tracker.istole.it:80 \
