5fb604919d5ae0954825223213602882382bc1b4
Christopher Rogers <slaxemulator@gmail.com>
2020-08-08 18:08:05 -0400

    Update youtube/upload.sh script to allow url input. If nothing is gave then it defaults to uploading list.txt ids.

--- youtube/upload.sh
+++ youtube/upload.sh
@@ -3,6 +3,7 @@
 #youtube-dl -j --flat-playlist https://www.youtube.com/user/chirikun/videos | jq -r '.id' > result.log
 
 check="yes"
+if [ "$1" == "" ]; then
 for i in $(tac list.txt); do
 	if [ "$check" == "yes" ]; then
 		i1="$(echo $i | grep \- | sed 's|^--||g' | sed 's|^-||g'| sed 's|--|-|g')"
@@ -19,3 +20,6 @@ for i in $(tac list.txt); do
 	fi
 tubeup youtube.com/watch?v=$i --metadata="collection:godaneinbox"
 done
+else
+tubeup $1 --metadata="collection:godaneinbox"
+fi
