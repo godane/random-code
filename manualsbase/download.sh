17611b8dd1aa2f064dffe26cfa0c4aa78c422aae
Christopher Rogers <slaxemulator@gmail.com>
2020-08-08 05:03:17 -0400

    Update manualsbase scripts to download then upload if files are not uploaded to IA.

--- manualsbase/download.sh
+++ manualsbase/download.sh
@@ -2,18 +2,20 @@
 
 
 #k="6"
-for k in 141; do #$(seq 140 142); do #$(seq 58 61); do
-for i in $(seq -w 000 999); do
-	file="${k}${i}.pdf"
+#for k in 141; do #$(seq 140 142); do #$(seq 58 61); do
+#for i in $(seq -w 000 999); do
+id="$1"
+	#file="${k}${i}.pdf"
+	file="${id}.pdf"
 	#way to grab title from html page
 	#curl -s http://www.manualsbase.com/manual/62000/1/1/1/ | grep title | sed 's|.*<title>||g' | sed 's| - ManualsBase.com.*||g' | head -1
 	if [ ! -f $file ]; then
-	pdfurl="$(curl -s http://www.manualsbase.com/manual/${k}${i}/1/1/1/ | grep -a pdf | sed 's|.*downloadmanual||g' | sed 's|.*#../|http://www.manualsbase.com/|g'  | sed 's|pdf".*|pdf|g' | grep -a ^http | head -1 | sed 's|#39;||g' | sed 's|&||g')"
+	pdfurl="$(curl -s http://www.manualsbase.com/manual/${id}/1/1/1/ | grep -a pdf | sed 's|.*downloadmanual||g' | sed 's|.*#../|http://www.manualsbase.com/|g'  | sed 's|pdf".*|pdf|g' | grep -a ^http | head -1 | sed 's|#39;||g' | sed 's|&||g')"
 	echo "$pdfurl"
-	echo "${k}${i}"
+	echo "${id}"
 	[ -f $file ] || wget -c --referer http://www.manualsbase.com/viewerjs/pdf.worker.js "$pdfurl" -O ${file}
 	fi
 	#[ -f $file ] || wget -c --referer http://www.manualsbase.com/viewerjs/pdf.worker.js http://www.manualsbase.com/manual/manualfileaspdf/${k}${i} -O ${file}
-done
-done
+#done
+#done
 
--- manualsbase/upload.sh
+++ manualsbase/upload.sh
@@ -4,18 +4,19 @@
 check="yes"
 c="$1"
 for i in $(seq ${c}00 ${c}99); do
-ls ${i}.pdf | while read file; do
 	id="manualsbase-id-${i}"
 	if [ "$check" == "yes" ]; then
 		url="archive.org/download/$id"
 		[ -f "$url" ] || wget -x -c $url
 		if [ -f "$url" ]; then
-			if [ "$(grep "$(basename "$file")" $url)" != "" ]; then
+			if [ "$(grep "$(basename "${i}.pdf")" $url)" != "" ]; then
 				echo "$file is in $url"
 				continue
 			fi
 		fi
 	fi
+bash download.sh $i
+ls ${i}.pdf | while read file; do
 	title1="$(curl -s http://www.manualsbase.com/manual/${i}/1/1/1/ | grep -a title | sed 's|.*<title>||g' | sed 's| - ManualsBase.com.*||g' | head -1)"
 	title="$title1"
 
