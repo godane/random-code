4b4071f3047ea293da0f282b7ed6a9489a946154
Christopher Rogers <slaxemulator@gmail.com>
2020-08-10 04:54:38 -0400

    Fix nicovideo.sh script in moving file.

--- youtube/nicovideo.sh
+++ youtube/nicovideo.sh
@@ -3,7 +3,8 @@
 url="$1"
 youtube-dl --external-downloader aria2c --external-downloader-args "-k 2M -x 16 -s 16 -j 16" "$url"
 
-filename="$(basename $url).mp4"
-cp *-${filename}.mp4 $HOME/.tubeup/downloads/${filename}.mp4
+
+ls *-$(basename $url).mp4 | while read file; do
+cp "${file}" $HOME/.tubeup/downloads/$(basename $url).mp4
 
 bash upload.sh $url
