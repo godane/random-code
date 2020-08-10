3c151bcc6c56b2e650add444f4437704498aa66f
Christopher Rogers <slaxemulator@gmail.com>
2020-08-10 04:59:01 -0400

    Fix nicovideo.sh again.

--- youtube/nicovideo.sh
+++ youtube/nicovideo.sh
@@ -6,5 +6,6 @@ youtube-dl --external-downloader aria2c --external-downloader-args "-k 2M -x 16 
 
 ls *-$(basename $url).mp4 | while read file; do
 cp "${file}" $HOME/.tubeup/downloads/$(basename $url).mp4
-
 bash upload.sh $url
+done
+
