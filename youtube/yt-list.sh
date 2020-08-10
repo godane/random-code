ac565734670e693918fe50d480c0a2375eda769a
Christopher Rogers <slaxemulator@gmail.com>
2020-08-09 20:38:31 -0400

    Update youtube/yt-list.sh to change list.txt filename as 2nd option. By default if not give its just list.txt.

--- youtube/yt-list.sh
+++ youtube/yt-list.sh
@@ -3,8 +3,15 @@
 #youtube-dl -j --flat-playlist https://www.youtube.com/user/chirikun/videos | jq -r '.id' > result.log
 
 url="$1"
+list="$2"
+if [ "$list" == "" ]; then
+	list="list.txt"
+else
+	list="$2"
+fi
+
 if [ "$url" != "" ]; then
-youtube-dl -j --flat-playlist "$url" | jq -r '.id' > list.txt
+youtube-dl -j --flat-playlist "$url" | jq -r '.id' > "$list"
 else
 	echo "$0 youtube-channel-url"
 fi
