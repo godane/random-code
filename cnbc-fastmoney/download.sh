eb730538245fea07c21c57e086395c073bd6e403
Christopher Rogers <slaxemulator@gmail.com>
2020-08-13 19:40:19 -0400

    cnbc-fastmoney/download.sh: Updated to use megaphone.fm rss feed to get mp3s.

--- cnbc-fastmoney/download.sh
+++ cnbc-fastmoney/download.sh
@@ -3,8 +3,9 @@
 y="2016"
 for m in $(seq -w 01 12); do
 	for d in $(seq -w 01 31); do
-		url="http://podcast.cnbc.com/FastMoney-${m}${d}${y:2:4}.mp3"
-		file="$(basename $url)"
+		# use megaphone rss feed for mp3s: https://feeds.megaphone.fm/fast-money
+		url="$(cat fast-money | grep -A15 ${m}/${d}/${y:2:4} | sed 's|.*mp3/||g' | sed 's|".*||g' | grep ^traf)"
+		file="FastMoney-${m}${d}${y:2:4}.mp3"
 		[ -d $y/$m ] || mkdir -p $y/$m
 		[ -f $y/$m/$file ] || wget -c $url -O $y/$m/$file
 	done
