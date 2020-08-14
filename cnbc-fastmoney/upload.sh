66887614749dde2e18bbfd8fee812e8cf7671bf9
Christopher Rogers <slaxemulator@gmail.com>
2020-08-13 21:36:35 -0400

    cnbc-fastmoney/upload.sh: Fix typo in id. Add check to make sure file exist and check to see if id is used.

--- cnbc-fastmoney/upload.sh
+++ cnbc-fastmoney/upload.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 
-y="2016"
+check="yes"
+for y in $(seq 2016 2019); do
 for m in $(seq -w 01 12); do
 	for d in $(seq -w 01 31); do
 		title="CNBC FastMoney Podcast ${y}-${m}-${d}"
 		basekeywords="CNBC FastMoney Podcast;"
-		id="cnbce-fastmoney-podcast-${y}-${m}-${d}"
+		id="cnbc-fastmoney-podcast-${y}-${m}-${d}"
 		file="$y/$m/FastMoney-${m}${d}${y:2:4}.mp3"
-	
+		if [ "$check" == "yes" ]; then
+			url="archive.org/download/$id"
+			[ -f "$url" ] || wget -x -c $url
+			if [ -f "$url" ]; then
+				if [ "$(grep "$(basename "$file")" $url)" != "" ]; then
+					echo "$file is in $url"
+					continue
+				fi
+			fi
+		fi
+		[ -f $file ] || continue
+		
 		ia upload $id "$file" \
 			--metadata="collection:godaneinbox" \
 			--metadata="mediatype:audio" \
 			--metadata="title:$title" \
 			--metadata="subject:${basekeywords}"
 	done
+done
 done
