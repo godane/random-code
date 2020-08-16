ae3bc4c430a3ae682c16a15a2a2f3655f3e57a98
Christopher Rogers <slaxemulator@gmail.com>
2020-08-15 22:20:42 -0400

    manualslib/download.sh: Use printPage option to get ocr pdf files of each page then use pdfunite to make one pdf.

--- manualslib/download.sh
+++ manualslib/download.sh
@@ -2,20 +2,25 @@
 
 id="$1"
 
-if [ ! -f $id.cbz ]; then
+if [ ! -f $id.pdf ]; then
 	for p in $(seq -w 0001 9999); do
 		[ -d $id ] || mkdir $id
-		purl="https://data2.manualslib.com/bigImage/$id/${p}?langCode=en"
-		pfile="$id/p${p}.png"
-		[ -f $pfile ] || wget -no-check-certificate -T 10 -c $purl -O $pfile -o $id-wget.log
+		purl="https://www.manualslib.com/printPage?manual_id=${id}&page=${p}"
+		pfile="$id/p${p}.pdf"
+		
+		#example code
+		#for i in $(seq -w 727 784); do wget --referer https://www.manualslib.com/ -T 5 "https://www.manualslib.com/printPage?manual_id=116&page=${i}_blank" -O p${i}.pdf; done
+
+		[ -f $pfile ] || wget --referer http://www.manualslib.com/ --no-check-certificate -T 10 -c $purl -O $pfile
 		find $id -size 12c -delete
+		find $id -size -4k -delete
 		find $id -empty -delete
 		[ -f $pfile ] || break
 	done
 	totalp="$(curl -L -s https://www.manualslib.com/manual/$id/a.html | grep -i 'print document' | sed 's|.*(||g' | sed 's| .*||g')"
-	if [  "$(find $id -name "*0${totalp}.png" -type f)" != "" ]; then
-		zip -r $id.cbz $id
-		if [ -d $id -a $id.cbz ]; then
+	if [  "$(find $id -name "*0${totalp}.pdf" -type f)" != "" ]; then
+		pdfunite $id/*.pdf ${id}.pdf
+		if [ -d $id -a $id.pdf ]; then
 			rm -rf $id
 		fi
 	else
