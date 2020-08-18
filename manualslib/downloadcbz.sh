f0f411173a40a89c07bc5afcfc034f1ac2b96b47
Christopher Rogers <slaxemulator@gmail.com>
2020-08-18 14:26:06 -0400

    manualslib/downloadcbz.sh: Now it checks if that last page is a Table of content page. If it is then the last page is one less.

--- manualslib/downloadcbz.sh
+++ manualslib/downloadcbz.sh
@@ -4,7 +4,13 @@ id="$1"
 
 if [ ! -f $id.cbz ]; then
 	totalp="$(curl -L -s https://www.manualslib.com/manual/$id/a.html | grep -i 'print document' | sed 's|.*(||g' | sed 's| .*||g')"
-	for p in $(seq 1 9999); do
+	if [ "$(curl -L -s https://www.manualslib.com/manual/$id/a.html?page=${totalp} | sed 's|><|>\n<|g' | grep '<h2>Table')" != "" ]; then
+		total=$(echo "${totalp} - 1" | bc)
+	else
+		total="${totalp}"
+	fi
+	if [ "$(echo $total | wc c)" -lt "4" ]; then
+	for p in $(seq 1 $total); do
 		if [ "$(echo $p | wc -c)" == "2" ]; then
 			p1="000${p}"
 		elif [ "$(echo $p | wc -c)" == "3" ]; then
@@ -21,21 +27,24 @@ if [ ! -f $id.cbz ]; then
 		[ -f $pfile ] || break
 	done
 
-	if [ "$(echo $totalp | wc -c)" == "2" ]; then
-		totalp1="000${totalp}"
-	elif [ "$(echo $totalp | wc -c)" == "3" ]; then
-		totalp1="00${totalp}"
-	elif  [ "$(echo $totalp | wc -c)" == "4" ]; then
-		totalp1="0${totalp}"
+	if [ "$(echo $total | wc -c)" == "2" ]; then
+		total1="000${total}"
+	elif [ "$(echo $total | wc -c)" == "3" ]; then
+		total1="00${total}"
+	elif  [ "$(echo $total | wc -c)" == "4" ]; then
+		total1="0${total}"
 	else
-		totalp1="{totalp}"
+		total1="{total}"
 	fi
-	if [  "$(find $id -name "p${totalp1}.png" -type f)" != "" ]; then
+	if [  "$(find $id -name "p${total1}.png" -type f)" != "" ]; then
 		zip -r $id.cbz $id
 		if [ -d $id -a $id.cbz ]; then
 			rm -rf $id
 		fi
 	else
 		echo "$id is incomplete" >> incomplete.txt
+	fi
+	else
+		echo "$id is more then 99 pages." >> incomplete.txt
 	fi
 fi
