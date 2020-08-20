c403a89ae7a8b6d20d3da0d82afc80e850ec2e8e
Christopher Rogers <slaxemulator@gmail.com>
2020-08-20 11:41:14 -0400

    kbs-news/upload.sh: Add check if statement.

--- kbs-news/upload.sh
+++ kbs-news/upload.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+check="yes"
 y="1987"
 for m in $(seq -w 01 12); do
 	for d in $(seq -w 01 31); do
@@ -8,7 +9,17 @@ for m in $(seq -w 01 12); do
 		id="kbs-news-video-${name}-${y}-${m}-${d}-1500k"
 		title="KBS News Video ${name} ${y}-${m}-${d}"
 		basekeywords="KBS News Video; ${name};"
-	
+		if [ "$check" == "yes" ]; then
+		url="archive.org/download/$id"
+		[ -f "$url" ] || wget -x -c $url
+		if [ -f "$url" ]; then
+			if [ "$(grep "$(basename "$file")" $url)" != "" ]; then
+				echo "$file is in $url"
+				continue
+			fi
+		fi
+		fi
+
 		lang="korean"
 		ia upload $id "$file" \
 			--metadata="collection:godaneinbox" \
