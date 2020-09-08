13b027a4611027751b879215289fe73defefca89
Christopher Rogers <slaxemulator@gmail.com>
2020-09-08 03:58:49 -0400

    kotaku/mirror.sh: Fixed getting urls from sitemap url.

--- kotaku/mirror.sh
+++ kotaku/mirror.sh
@@ -7,7 +7,7 @@ for m in 12; do
 		url="https://$website/sitemap_bydate.xml?startTime=${y}-${m}-${d}T00:00:00&endTime=${y}-${m}-${d}T23:59:59"
 		dir="$y/$m/$d"
 		[ -d $dir ] || mkdir -p $dir
-		curl -s "$url" | sed 's|><|>\n<|g' | grep loc | sed 's|.*>http|http|g' | sed 's|<.*||g' | sort | uniq > $dir/index.txt
+		curl -s "$url" |  sed "s|.*\[||g" | sed "s|].*||g" | sed 's|.*http|http|g' | sed 's|<.*||g' | sort | uniq > $dir/index.txt
 		echo "$url" >> $dir/index.txt
 		wget -x -i $dir/index.txt --warc-file=$dir/${website}-sitemap-${y}-${m}-${d}-$(date +%Y%m%d) --warc-cdx -o $dir/wget.log -P $dir
 	done
