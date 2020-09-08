a0d12a392d18c38a79b812f1c8f134971453be38
Christopher Rogers <slaxemulator@gmail.com>
2020-09-08 03:44:08 -0400

    kotaku/upload.sh: Add month and day for loop. Also add check if statement.

--- kotaku/upload.sh
+++ kotaku/upload.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
+y="2016"
+for m in 12; do
+for d in $(seq -w 01 31); do
 website="kotaku.com"
-subject="sitemap-${y}-${m}-${i}"
+subject="sitemap-${y}-${m}-${d}"
 date="20200908"
 warcfile="$website-$subject-${date}"
 dir="$y/$m/$d"
 for file in $dir/wget.log $dir/index.txt $dir/${warcfile}.warc.gz $dir/${warcfile}.cdx; do
 
 	id="$warcfile"
+	if [ "$check" == "yes" ]; then
+		url="archive.org/download/$id"
+		[ -f "$url" ] || wget -x -c $url
+		if [ -f "$url" ]; then
+			if [ "$(grep "$(basename "$file")" $url)" != "" ]; then
+				echo "$file is in $url"
+				continue
+			fi
+		fi
+	fi
 	desc="This is a panic download of $website $subject as of $(date -d $date +%Y-%m-%d)."
 	basekeywords="$website; archiveteam;"
 
@@ -16,4 +29,6 @@ for file in $dir/wget.log $dir/index.txt $dir/${warcfile}.warc.gz $dir/${warcfil
 		--metadata="mediatype:web" \
 		--metadata="description:$desc" \
 		--metadata="subject:${basekeywords}"
+done
+done
 done
