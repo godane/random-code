3a1e4301b9fa126c85d9872ff10187c68fd11096
Christopher Rogers <slaxemulator@gmail.com>
2020-09-28 20:59:54 -0400

    Add soundcloud/upload.sh script.

---
+++ soundcloud/upload.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#youtube-dl -j --flat-playlist https://www.youtube.com/user/chirikun/videos | jq -r '.id' > result.log
+
+check="yes"
+if [ "$1" == "" ]; then
+for i in $(tac list.txt); do
+	if [ "$check" == "yes" ]; then
+		#i1="$(echo $i | grep \- | sed 's|^--||g' | sed 's|^-||g'| sed 's|--|-|g')"
+		i1="$(curl -s $i | sed 's|,|\n|g' |  grep api.soundcloud.com/tracks | sed 's|.*/||g' | sed 's|".*||g')"
+		#if [ "$i1" == "" ]; then
+		#    url="archive.org/download/youtube-${i}"
+		#else
+		    url="archive.org/download/soundcloud-${i1}"
+		#fi
+		echo $url
+		[ -f "$url" ] || wget -x -c $url
+		if [ -f "$url" ]; then
+			continue
+		fi
+	fi
+tubeup $i --metadata="collection:godaneinbox"
+done
+else
+tubeup $1 --metadata="collection:godaneinbox"
+fi
