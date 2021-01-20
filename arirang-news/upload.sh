869f2eca63a227365f83c67fefdab8a2a201242c
Christopher Rogers <slaxemulator@gmail.com>
2021-01-20 04:42:14 -0500

    Update arirang-news upload script.

--- arirang-news/upload.sh
+++ arirang-news/upload.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
-
+check="yes"
 for i in $1; do
 	code="RNews"
 	pcode="RADR0125"
-	file="${i}.m4a"
+	[ -f ${i}.m4a ] || continue
 	id="arirang-audio-aircheck-$code-$pcode-id-$i"
-	
+	wget -c "http://www.arirang.com/Player/News_Script.asp?code=${code}&AKey=${i}" -O ${i}.html
+	find -name "${i}.html" -size 0 -delete
+	ls ${i}.* | while read file; do
+	if [ "$check" == "yes" ]; then
+	url="archive.org/download/$id"
+	[ -f "$url" ] || wget -x -c $url
+	if [ -f "$url" ]; then
+		if [ "$(grep "$(basename "$(echo "${file}" | sed "s|\[.*||g")")" $url)" != "" ]; then
+			echo "$file is in $url"
+			continue
+		fi
+	fi
+	fi
 	basekeywords="Arirang; Radio News; Aircheck;"
 	creator="Arirang"
 	lang="english"
@@ -17,4 +29,5 @@ for i in $1; do
 		--metadata="creator:$creator" \
 		--metadata="subject:${basekeywords}" \
 		--metadata="language:$lang"
+	done
 done
