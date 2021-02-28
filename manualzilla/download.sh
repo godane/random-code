69b96947eb4b1fe76b9e41b99442cfa9bda3fab8
Christopher Rogers <slaxemulator@gmail.com>
2021-02-28 08:27:10 -0500

    manualzilla: Fix url grab of pdf in download script.

--- manualzilla/download.sh
+++ manualzilla/download.sh
@@ -5,7 +5,7 @@ id="$1"
 file="${id}.pdf"
 if [ ! -f $file ]; then
 [ -f ${id}.html ] || wget -U firefox -e robots=off -c https://manualzilla.com/doc/${id}/ -O ${id}.html
-url="$(cat ${id}.html | grep pdf | sed 's|%2F|/|g' | sed 's|%3D|=|g' | sed 's|%3F|?|g'  | sed 's|%26|\&|g' | sed 's|\&img=.*||g' | sed 's|.*file=//|http://|g' | grep ^http)"
+url="$(cat ${id}.html | grep pdf | grep iframe | sed 's|%2F|/|g' | sed 's|%3D|=|g' | sed 's|%3F|?|g'  | sed 's|%26|\&|g' | sed 's|\&img=.*||g' | sed 's|.*file=//|http://|g' | grep ^http)"
 #echo "sleep 5"
 #sleep 5
 echo "downloading ${file}"
