42784b4957698b0c02e1d4cf5c972f56b5e012e4
Christopher Rogers <slaxemulator@gmail.com>
2021-12-19 18:50:57 -0500

    shoutengine: Update download script

--- shoutengine/download.sh
+++ shoutengine/download.sh
@@ -2,11 +2,15 @@
 
 #13xxxxx to 15xxxxx are done
 #http://shoutengine.com/a/a-100695.mp3
-k="10"
-for i in $(seq -w 0000 0999); do
-	url="http://shoutengine.com/a/${k}${i}.mp"
-	dir="${k}xxxxx"
+k="100"
+start="${k}000"
+end="${k}999"
+for i in $(seq -w 000 999); do
+	url="http://shoutengine.com/a/a-${i}.mp3"
+	dir="${k}xxx/${i}"
 	[ -d $dir ] || mkdir -p $dir
 	file="$(basename $url)"
-	[ -d $file ] ||  wget --content-disposition $url -P $dir
+	if [ "$(find $dir -name "*.mp3" -type f)" == "" ]; then
+		wget --content-disposition $url -P $dir
+	fi
 done
