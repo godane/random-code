1936385bf199a43c47cd2f6199c6d699dc4d4346
Christopher Rogers <slaxemulator@gmail.com>
2023-06-20 17:31:04 -0400

    cia-readingroom: Fix index.sh

--- cia-readingroom/index.sh
+++ cia-readingroom/index.sh
@@ -2,7 +2,7 @@
 
 y="1977"
 endy="$(echo "${y} + 1" | bc)"
-url="https://www.cia.gov/readingroom/search/site?page=1&f%5B0%5D=dm_field_release_date%3A%5B${y}-01-01T00%3A00%3A00Z%20TO%20${endy}-01-01T00%3A00%3A00Z%5D"
+url="https://www.cia.gov/readingroom/search/site?f%5B0%5D=dm_field_release_date%3A%5B${y}-01-01T00%3A00%3A00Z%20TO%20${endy}-01-01T00%3A00%3A00Z%5D"
 
 [ -f index.html ] || wget -c $url -O index.html
 cat index.html | grep href= | sed 's|.*href="||g' | sed 's|".*||g' | grep ^http | grep readingroom/document | sed 's|.*/||g' > list.txt
